30d055087988941cb22f93a4cd332c924b3446ac
kisekitw <kisekitw@gmail.com>
2018-06-05 14:57:08 +0000

    Refactoring:Merging Services

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MakeService } from '../../services/make.service';
-import { FeatureService } from '../../services/feature.service';
+import { VehicleService } from '../../services/vehicle.service';
 
 @Component({
   selector: 'app-vehicle-form',
@@ -13,15 +12,14 @@ export class VehicleFormComponent implements OnInit {
   models:any[];
   vehicle:any = {};
   constructor(
-    private makeService:MakeService,
-    private featureService:FeatureService) { }
+    private vehicleService:VehicleService) { }
 
   ngOnInit() {
-    this.makeService.getMakes().subscribe(makes => {
+    this.vehicleService.getMakes().subscribe(makes => {
       this.makes = makes;
     });
 
-    this.featureService.getFeatures().subscribe(features => {
+    this.vehicleService.getFeatures().subscribe(features => {
       this.features = features;
     });
   }
